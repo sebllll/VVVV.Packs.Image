af5d0dfa0936324dc2d5b6b341935003fa3e5cda
sebl <sebl@schnellebuntebilder.de>
2017-09-08 15:05:01 +0200

    fixed some more references

--- src/nodes/plugins/Image/OpenCV/src/Filters/Queue.cs
+++ src/nodes/plugins/Image/OpenCV/src/Filters/Queue.cs
@@ -16,6 +16,7 @@ using VVVV.PluginInterfaces.V1;
 using VVVV.PluginInterfaces.V2;
 using VVVV.Utils.VColor;
 using VVVV.Utils.VMath;
+using VVVV.Utils;
 
 using VVVV.Nodes.Generic;
 
@@ -24,7 +25,7 @@ using VVVV.Nodes.Generic;
 namespace VVVV.CV.Nodes
 {
     #region PluginInfo
-    [FilterInstance("Queue", Version = "", Tags = "", Help = "A Queue for CV.Image", Author = "sebl")]
+    [FilterInstance("Queue", Version = "FIFO", Tags = "", Help = "A Queue for CV.Image", Author = "sebl")]
     #endregion PluginInfo
     public class QueueNode : IFilterInstance
     {
@@ -46,9 +47,25 @@ namespace VVVV.CV.Nodes
         [Output("Queue Count")]
         public int FOutCount;
 
+        [Output("Images Per Frame")]
+        public int FOutFrames;
+
         int QueueCount = 0;
 
         private List<CVImage> FrameQueue = new List<CVImage>();
+
+        private CVImage LastImage;
+
+        int FFrames = 0;
+        public int Frames
+        {
+            get
+            {
+                int f = FFrames;
+                FFrames = 0;
+                return f;
+            }
+        }
 
         #endregion fields and pins
 
@@ -57,6 +74,7 @@ namespace VVVV.CV.Nodes
         {
             FrameQueue = new List<CVImage>();
             FOutput.Image.Initialise(FInput.Image.ImageAttributes);
+            LastImage.Initialise(FInput.Image.ImageAttributes);
         }
 
         public override bool IsFast()
@@ -64,10 +82,17 @@ namespace VVVV.CV.Nodes
             return false;
         }
 
-        this.FProcessor[i].FlagForProcess();
+        void Flagger()
+        {
+            FlagForProcess();
+        }
 
         public override void Process()
         {
+            if (LastImage != FInput.Image)
+            {
+
+            }
 
             if (FReset)
             {
@@ -84,12 +109,14 @@ namespace VVVV.CV.Nodes
 
             if (FDoInsert)
             {
-
                 CVImage copy = new CVImage();
                 copy.Initialise(FInput.ImageAttributes);
                 ImageUtils.CopyImage(FInput.Image, copy);
-                FrameQueue.Insert(0,copy);
+                FrameQueue.Insert(0, copy);
+                //FrameQueue.Add(copy);
             }
+
+            LastImage = FInput.Image;
 
             FInput.ReleaseForReading(); // not sure if needed
 
@@ -104,16 +131,141 @@ namespace VVVV.CV.Nodes
                     img.Dispose();
 
                 FrameQueue.RemoveRange(FFrameCount, tooMuch);
+                //FrameQueue.RemoveRange(0, tooMuch);
             }
-            
+
             QueueCount = FrameQueue.Count();
 
             // set OutPuts
 
             FOutCount = QueueCount;
-           
-            FOutput.Send(FrameQueue[FInIndex % QueueCount]);
 
+            var idx = FInIndex % QueueCount;
+
+            FOutput.Send(FrameQueue[idx]);
+        }
+
+    }
+
+    #region PluginInfo
+    [FilterInstance("Queue", Version = "LIFO", Tags = "", Help = "A Queue for CV.Image with Last-in, First Out", Author = "sebl")]
+    #endregion PluginInfo
+    public class QueueLifoNode : IFilterInstance
+    {
+        #region fields and pins
+
+        [Input("Frame Count")]
+        public int FFrameCount = 1;
+
+        [Input("Insert")]
+        public bool FDoInsert = false;
+
+        [Input("Reset")]
+        public bool FReset = false;
+
+        [Input("Index")]
+        public int FInIndex = 0;
+
+
+        [Output("Queue Count")]
+        public int FOutCount;
+
+        [Output("Images Per Frame")]
+        public int FOutFrames;
+
+        int QueueCount = 0;
+
+        private List<CVImage> FrameQueue = new List<CVImage>();
+
+        private CVImage LastImage;
+
+        int FFrames = 0;
+        public int Frames
+        {
+            get
+            {
+                int f = FFrames;
+                FFrames = 0;
+                return f;
+            }
+        }
+
+        #endregion fields and pins
+
+
+        public override void Allocate()
+        {
+            FrameQueue = new List<CVImage>();
+            FOutput.Image.Initialise(FInput.Image.ImageAttributes);
+            LastImage.Initialise(FInput.Image.ImageAttributes);
+        }
+
+        public override bool IsFast()
+        {
+            return false;
+        }
+
+        void Flagger()
+        {
+            FlagForProcess();
+        }
+
+        public override void Process()
+        {
+            if (LastImage != FInput.Image)
+            {
+
+            }
+
+            if (FReset)
+            {
+                foreach (CVImage img in FrameQueue)
+                    img.Dispose();
+
+                FrameQueue.Clear();
+            }
+
+            // push frames
+
+            if (!FInput.LockForReading()) // not sure if needed
+                return;
+
+            if (FDoInsert)
+            {
+                CVImage copy = new CVImage();
+                copy.Initialise(FInput.ImageAttributes);
+                ImageUtils.CopyImage(FInput.Image, copy);
+                //FrameQueue.Insert(0, copy);
+                FrameQueue.Add(copy);
+            }
+
+            LastImage = FInput.Image;
+
+            FInput.ReleaseForReading(); // not sure if needed
+
+
+            // trim queue
+
+            if (FFrameCount >= 0 && FrameQueue.Count > FFrameCount)
+            {
+                var tooMuch = FrameQueue.Count - FFrameCount;
+                var toDispose = FrameQueue.GetRange(FFrameCount, tooMuch);
+                foreach (CVImage img in toDispose)
+                    img.Dispose();
+
+                //FrameQueue.RemoveRange(FFrameCount, tooMuch);
+                FrameQueue.RemoveRange(0, tooMuch);
+            }
+
+            QueueCount = FrameQueue.Count();
+
+            // set OutPuts
+
+            FOutCount = QueueCount;
+
+            var idx = FInIndex % QueueCount;
+
+            FOutput.Send(FrameQueue[idx]);
         }
 
     }
